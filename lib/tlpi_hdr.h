19710d0380ef100d214f748876ea7a556a9f1a14
dingpeilong <77676182@qq.com>
2018-04-16 17:50:05 +0800

    update

---
+++ lib/tlpi_hdr.h
@@ -0,0 +1,34 @@
+/* tlpi_hdr.h
+
+   Standard header file used by nearly all of our example programs.
+*/
+#ifndef TLPI_HDR_H
+#define TLPI_HDR_H
+
+#include <sys/types.h>  /* Type definitions used by many programs */
+#include <stdio.h>      /* Standard I/O functions */
+#include <stdlib.h>     /* Prototypes of commonly used library functions,
+                           plus EXIT_SUCCESS and EXIT_FAILURE constants */
+#include <unistd.h>     /* Prototypes for many system calls */
+#include <errno.h>      /* Declares errno and defines error constants */
+#include <string.h>     /* Commonly used string-handling functions */
+
+#include "error_functions.h"  /* Declares our error-handling functions */
+
+/* Unfortunately some UNIX implementations define FALSE and TRUE -
+   here we'll undefine them */
+
+#ifdef TRUE
+#undef TRUE
+#endif
+
+#ifdef FALSE
+#undef FALSE
+#endif
+
+typedef enum { FALSE, TRUE } Boolean;
+
+#define min(m,n) ((m) < (n) ? (m) : (n))
+#define max(m,n) ((m) > (n) ? (m) : (n))
+
+#endif /* TLPI_HDR_H */
